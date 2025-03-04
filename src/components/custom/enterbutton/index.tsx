7f51b57f2ad3b9313dba0a2b729d96b5f902edbf
ImfanAi <azizamirul086@gmail.com>
2025-03-04 07:14:54 -0600

    main app linked

--- src/components/custom/enterbutton/index.tsx
+++ src/components/custom/enterbutton/index.tsx
@@ -3,10 +3,12 @@ import "./index.css";
 export default function EnterButton() {
    return(
       <>
-         <button className="shiny-cta px-2 py-2 sm:px-4 sm:py-2 md:px-6 md:py-2 lg:py-2 lg:px-6 xl:py-[15px]"
-         >
-            <span className="font-[family-name:var(--font-man-rope)] font-bold md:text-lg sm:text-md text-sm">&nbsp;Coming&nbsp;&nbsp;Soon&nbsp;</span>
-         </button>
+         <a href={`${process.env.NEXT_PUBLIC_MAIN_APP_URL}`}>
+            <button className="shiny-cta px-2 py-2 sm:px-4 sm:py-2 md:px-6 md:py-2 lg:py-2 lg:px-6 xl:py-[15px]"
+            >
+               <span className="font-[family-name:var(--font-man-rope)] font-bold md:text-lg sm:text-md text-sm">&nbsp;Launch&nbsp;&nbsp;App&nbsp;</span>
+            </button>
+         </a>
       </>
    );
 }
